a4926092b22ea43056e9fba50964315c6c4b863c
MarkShileynikov <mark.shiley11@gmail>
2024-03-22 22:21:18 +0300

    fixed book search

--- app/src/main/java/com/example/mybookapplication/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/mybookapplication/presentation/search/SearchFragment.kt
@@ -3,7 +3,6 @@ package com.example.mybookapplication.presentation.search
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +10,7 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +19,8 @@ import com.example.mybookapplication.domain.entity.Book
 import com.example.mybookapplication.presentation.search.adapter.BookListAdapter
 import com.example.mybookapplication.presentation.search.adapter.GenreAdapter
 import com.example.mybookapplication.presentation.util.ViewState
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment(), OnItemClickedListener {
@@ -42,7 +41,6 @@ class SearchFragment : Fragment(), OnItemClickedListener {
         val view = inflater.inflate(R.layout.fragment_search, container, false)
 
         setUpViews(view)
-
         return view
     }
     private fun setUpViews(view : View) {
@@ -52,33 +50,51 @@ class SearchFragment : Fragment(), OnItemClickedListener {
 
         setUpGenreList()
 
-        searchView.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        setUpSearch(view)
 
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val length = s?.length ?: 0
-                if (length == 0) {
-                    setUpGenreList()
-                    view.findViewById<TextView>(R.id.genresHeader).visibility = View.VISIBLE
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                val length = s?.length ?: 0
-                if (length > 0) {
-                    viewModel.fetchBooksByTitleOrAuthor(s.toString())
-                    observeBooks(view)
-                }
-            }
-        })
     }
 
     private fun setUpGenreList() {
         val genres : List<String> = listOf("Фэнтези", "Художественная литература", "Классика")
         genreAdapter = GenreAdapter(genres, this)
         recyclerView.adapter = genreAdapter
+    }
+
+    fun setUpSearch(view : View) {
+        searchView.addTextChangedListener(object : TextWatcher {
+            private var searchJob: Job? = null
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val query = s?.toString() ?: ""
+                searchJob?.cancel()
+                if (query.isNotBlank()) {
+                    searchJob = lifecycleScope.launch {
+                        delay(300)
+                        viewModel.fetchBooksByTitleOrAuthor(query)
+                        viewModel.viewState.collect {
+                            when(it) {
+                                is ViewState.Success -> {
+                                    view.findViewById<TextView>(R.id.genresHeader).visibility = View.GONE
+                                    setUpBookList(it.data)
+                                }
+                                is ViewState.Loading -> {
+                                    //TODO
+                                }
+                                is ViewState.Failure -> {
+                                    //TODO
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    view.findViewById<TextView>(R.id.genresHeader).visibility = View.VISIBLE
+                    setUpGenreList()
+                }
+            }
+        })
     }
 
     private fun setUpBookList(books : List<Book>) {
@@ -90,27 +106,5 @@ class SearchFragment : Fragment(), OnItemClickedListener {
         val bundle = Bundle()
         bundle.putString(GENRE_KEY, genre)
         findNavController().navigate(R.id.to_book_list_screen, bundle)
-    }
-
-    private fun observeBooks(view : View) {
-        lifecycleScope.launch{
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.viewState.collect {
-                    when(it) {
-                        is ViewState.Success -> {
-                            view.findViewById<TextView>(R.id.genresHeader).visibility = View.GONE
-                            setUpBookList(it.data)
-                        }
-                        is ViewState.Loading -> {
-                            //TODO
-                        }
-                        is ViewState.Failure -> {
-                            //TODO
-                        }
-                    }
-                }
-            }
-
-        }
     }
 }
